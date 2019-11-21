7c39ace64e0cd4526783afcd9305195190f16410
wengy12 <wengy12>
2019-11-20 23:51:06 -0500

    sad

---
+++ TMA 1/part1/resume.xsl
@@ -0,0 +1,11 @@
+<?xml version="1.0"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <html>
+  <body>
+  <h2>Resume</h2>
+    <p>
+        <xsl:value-of select="resume/genereal/name"/>
+    </p>
+  </body>
+  </html>
+</xsl:stylesheet>
---
+++ TMA 1/shared/myscripts.js
@@ -0,0 +1,21 @@
+
+
+//highliting the code
+w3CodeColor();
+
+
+//collapsable
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
